9469804213918b50df17af445a0650273fe325a2
he-andy <42684804+he-andy@users.noreply.github.com>
2023-05-05 18:04:50 -0400

    Update Home.js

--- src/Home.js
+++ src/Home.js
@@ -16,7 +16,8 @@ class Home extends React.Component {
         <main>
           <h1> A software engineer 💻, photographer 📷, and casual matcha enthusiast 🍵 Interested in machine learning, computer vision, and theoretical computer science.</h1>
           <h1> Currently a second-year student studying Math & CS @ Cornell University in Ithaca, NY where the time is {this.state.time}.</h1>
-          <h1> Here are my <a href="https://www.instagram.com/a.ndyhe/">Instagram</a>, <a href="https://www.linkedin.com/in/ah734/">LinkedIn</a>, and <a href="https://github.com/he-andy/">GitHub</a>. You can reach me at my <a href="mailto:ah734@cornell.edu">email</a>.</h1>
+          <h1> Here are my <a href="https://www.instagram.com/a.ndyhe/">Instagram</a>, <a href="https://www.linkedin.com/in/ah734/">LinkedIn</a>, and <a href="https://github.com/he-andy/">GitHub</a>.</h1>
+          <h1> You can contact me for inquiries and bookings through my <a href="mailto:ah734@cornell.edu">email</a>. </h1>
           <h1> Here is my resume.</h1>
         </main >
 
